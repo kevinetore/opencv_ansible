670fc7dbb5820e7c38a011d52b4e0261ef74097a
Kevin Etore <info@etore.nl>
2023-01-22 20:26:22 +0000

    Make them run on Odroid N2

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -i
 
 if ! command -v ansible > /dev/null; then
+    sudo apt update -y
+    sudo apt install -y python3.8-distutils
+fi
+
+if ! command -v ansible > /dev/null; then
     wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py
     python3 get-pip.py --user
     python3 -m pip install --user ansible
