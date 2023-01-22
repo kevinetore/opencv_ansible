ad34ac22750531b225f46a08bc241a9291d6e404
Kevin Etore <info@etore.nl>
2023-01-22 20:41:59 +0100

    code improvements made to Ansible playbooks

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -i
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3 get-pip.py --user
 
-python3 -m pip install --user ansible
-echo "export PATH='~/.local/bin:$PATH'" >> ~/.bashrc
-source ~/.bashrc
+if ! command -v ansible > /dev/null; then
+    wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py
+    python3 get-pip.py --user
+    python3 -m pip install --user ansible
+    echo "export PATH='~/.local/bin:$PATH'" >> ~/.bashrc
+    source ~/.bashrc
+fi
 
 ansible-playbook -i hosts playbook.yml --ask-become-pass
 source ~/.bashrc
